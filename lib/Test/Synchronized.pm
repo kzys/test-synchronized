b9a871e2d4c3b8718bbe99d9e7ad79169111788c
KATO Kazuyoshi <kzys@8-p.info>
2009-12-07 12:30:11 +0900

    POD.

--- lib/Test/Synchronized.pm
+++ lib/Test/Synchronized.pm
@@ -34,7 +34,9 @@ Test::Synchronized
 
 =head1 DESCRIPTION
 
-prove's "-j" option is great.
+"prove -j9" is fast. But your tests are parallel-safe?
+
+Test::Synchronized provides simple lock system for your tests. If your tests includes some parallel-unsafe tests, You can use "prove -j9" with Test::Synchronized to speed up your development.
 
 =head1 AUTHOR
 
