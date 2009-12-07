db8491edbaaad80bd9839142ad3f58084738207e
KATO Kazuyoshi <kzys@8-p.info>
2009-12-07 11:09:27 +0900

    POD.

--- lib/Test/Synchronized.pm
+++ lib/Test/Synchronized.pm
@@ -18,3 +18,31 @@ sub import {
 }
 
 1;
+
+=head1 NAME
+
+Test::Synchronized
+
+=head1 SYNOPSIS
+
+  use Test::More tests => 1;
+  use Test::Synchronized;
+  
+  ...
+  
+  ok(cant_run_with_other_tests());
+
+=head1 DESCRIPTION
+
+prove's "-j" option is great.
+
+=head1 AUTHOR
+
+KATO Kazuyoshi E<lt>kzys@8-p.infoE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
